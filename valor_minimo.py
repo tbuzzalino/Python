985f6934eeb9089d80c63729a32b1822f9e95955
Tomas Buzzalino <54788347+tbuzzalino@users.noreply.github.com>
2020-08-19 14:58:17 -0300

    Add files via upload
    Con este script podemos saber cual es el valor minimo de 3 numeros que ingrasamos.

---
+++ valor_minimo.py
@@ -0,0 +1,13 @@
+# Viendo que numero es mas bajo
+
+""" Encontrar el valor minimo entre 3 valores"""
+
+num1 = int(input('Ingresa el primer numero: '))
+num2 = int(input('Ingresa el segundo numero: '))
+num3 = int(input('Ingresa el tercer numero: '))
+
+minimo = min(num1,num2,num3)
+
+print('El valor minimo de los numeros que ingresaste es', minimo ,'.')        
+
+
